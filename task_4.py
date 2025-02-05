9e870e879576cae4bf1595107a64115146fca6e2
alexsandr20082 <yxo1981@gmail.com>
2025-02-05 14:05:35 +0000

    8 работа

--- task_4.py
+++ task_4.py
@@ -4,18 +4,18 @@ import matplotlib.pyplot as plt
 
 t = np.arange(-5,5,0.01)
 
-def diff_func(v,t):
-    y,t  = w
+def diff_func(w,t):
+    y,v  = w
     dy_dt = v 
     dv_dt = -4*v - 5*y
     return dy_dt, dv_dt
 
 y0 = 4
-dy0_dt = -1
-w = y0,dy0_dt
+v0 = -1
+w0 = y0,v0
 
 
-sol = odeint(diff_func,w,t)
+sol = odeint(diff_func,w0,t)
 
 
 	
